f27225e56811274f116fd3c47e24697017dca06d
qer <wbxl2000@outlook.com>
2021-04-14 18:54:14 +0800

    chore: import Mattter-js

--- src/pages/create/components/canvasboard.js
+++ src/pages/create/components/canvasboard.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BoardWapper } from '../style';
+import Matter from 'matter-js';
 export default class CanvasBoard extends React.Component {
     render() {
         return (
